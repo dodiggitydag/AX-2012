3ae5c6e37efc6d25b013828a89a6a8933fb3b520
dodiggitydag <32473582+dodiggitydag@users.noreply.github.com>
2018-09-06 21:14:05 -0500

    Add files via upload

---
+++ Check DLL Versions.ps1
@@ -0,0 +1,3 @@
+﻿
+.\Get-AxDllVersions.ps1 -ComputerName (Get-Content "C:\Users\dcalafell\Desktop\PowerShell\Computers - All.txt")  | Export-csv "c:\temp\AX_DLL_Versions.csv" -NoTypeInformation
+#.\Get-AxDllVersions.ps1 -ComputerName ("AxTrain1","DaxDevMca1") | Export-csv "c:\temp\AX_DLL_Versions.csv" -NoTypeInformation
