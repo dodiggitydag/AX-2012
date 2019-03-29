2027beca889c28b35b285de3f3f6506fc7327d5f
Dag Calafell, III <32473582+dodiggitydag@users.noreply.github.com>
2019-03-29 00:09:07 -0500

    Fixed bug on updating field IDs

--- Fix table and field IDs to match AX model db.sql
+++ Fix table and field IDs to match AX model db.sql
@@ -17,6 +17,7 @@
  *   Objects that are new in AOT will get created in SQL dictionary when synchonisation happens
  *
  * History
+ *   2019-03-27 Dag Calafell  Fixed an issue with duplicate field-level records in SqlDictionary table (Steps 4 & 5)
  *   2018-05-18 Dag Calafell  Now it fixes the case where field ID is the same but name is different
  *   2018-05-18 Dag Calafell  Added step to remove duplicates in SqlDictionary
  *   2018-05-17 Dag Calafell  Initial code
@@ -38,6 +39,7 @@ FROM AX2012DB.dbo.SYSTEMSEQUENCES
 
 ----------------------------------------------------------------------------------------------
 -- Step 1: Check and fix any duplicates in SqlDictionary.
+--     Uses Name and SQLName to determine uniqueness
 ----------------------------------------------------------------------------------------------
 PRINT 'Step 1';
 
@@ -48,7 +50,7 @@ INTO #RecordsWithDuplicateTableIds
 FROM AX2012DB.dbo.SQLDICTIONARY
 WHERE SQLDICTIONARY.FIELDID = 0
 
--- Remove the non-duplicates from our list
+-- Remove the non-duplicates from the list
 DELETE #RecordsWithDuplicateTableIds
 WHERE RN = 1
 
@@ -59,16 +61,15 @@ WHERE TableId IN (SELECT TableId FROM #RecordsWithDuplicateTableIds)
 DROP TABLE #RecordsWithDuplicateTableIds
 
 
-
-USE AX2012DB_Model
-GO
-
 ----------------------------------------------------------------------------------------------
 -- Step 2 Find tables in SqlDictionary that have same name as in AOT but different ID and
 --   update SystemSequences.TabID = TableID in Modelstore.  The SystemSequences table holds
 --   the next available record ID block for each table.  The AOS actually consumes blocks of
 --   RecId's, usually 256 at a time, and so the AOS must not be running.
 ----------------------------------------------------------------------------------------------
+USE AX2012DB_Model
+GO
+
 PRINT 'Step 2';
 
 WITH t AS (
@@ -81,7 +82,7 @@ WITH t AS (
         s.NAME AS sName,
         s.SQLNAME
     FROM ModelElementData md, ModelElement m
-    LEFT OUTER JOIN AX2012DB..SQLDictionary s
+    LEFT OUTER JOIN AX2012DB.dbo.SQLDictionary s
         ON upper(m.NAME) collate Latin1_General_CI_AS = s.NAME
     WHERE m.ElementType = 44 -- UtilElementType::Table
         AND m.elementhandle = md.elementhandle
@@ -139,7 +140,7 @@ WITH t AS  (
         s.NAME AS sName,
         s.SQLNAME
     FROM modelelementdata md, ModelElement m
-    LEFT OUTER JOIN AX2012DB..SQLDictionary s
+    LEFT OUTER JOIN AX2012DB.dbo.SQLDictionary s
     ON (s.TABLEID * -1) = m.AxId
     WHERE m.ElementType = 44 -- UtilElementType::Table
         AND m.elementhandle = md.elementhandle
@@ -169,30 +170,43 @@ WITH t AS (
         s.RECID,
         M.ParentId,
         s.TableId,
-        s.FIELDID,
+        s.FieldID,
         S.NAME,
         s.SQLNAME
     FROM ModelElement m
-    LEFT OUTER JOIN AX2012DB..SQLDICTIONARY s
+    LEFT OUTER JOIN AX2012DB.dbo.SQLDICTIONARY s
         ON m.ParentId = s.TABLEID
         AND s.NAME =  upper(m.NAME) collate Latin1_General_CI_AS
     WHERE m.ElementType = 42 -- UtilElementType::TableField
         AND (s.ARRAY = 1 OR s.ARRAY IS NULL)
-        AND (s.FIELDID > 0 OR s.FIELDID IS NULL)
-        AND s.FIELDID != m.AxId
+        AND (s.FieldID > 0 OR s.FieldID IS NULL)
+        AND s.FieldID != m.AxId
 )
 UPDATE AX2012DB.dbo.SQLDICTIONARY
 SET FIELDID = (t.axid * -1) -- Set to a negative number but correct ID
 FROM t join AX2012DB.dbo.SQLDICTIONARY s
 ON upper(t.mName) collate Latin1_General_CI_AS = s.NAME
-    AND s.FIELDID <> 0
-    AND s.TABLEID = t.ParentId
+    AND s.FieldID <> 0
+    AND s.TableId = t.ParentId
 GO
 
+USE AX2012DB
+
 -- Reverse the negative to positive
-UPDATE AX2012DB.dbo.SQLDICTIONARY
-SET FIELDID = (FIELDID * -1)
-WHERE FIELDID < 0
+UPDATE SQLDICTIONARY
+SET FieldID = (FieldID * -1)
+WHERE FieldID < 0
+AND NOT EXISTS (
+	SELECT *
+	FROM SQLDICTIONARY d
+	WHERE d.TableID = SQLDICTIONARY.TableID
+	  AND d.FieldID = SQLDICTIONARY.FieldID * -1
+)
+GO
+
+DELETE FROM SQLDICTIONARY
+WHERE FieldID < 0
+
 GO
 
 ----------------------------------------------------------------------------------------------
@@ -202,33 +216,40 @@ GO
 ----------------------------------------------------------------------------------------------
 PRINT 'Step 5';
 
+USE AX2012DB_model
+
+
 WITH t AS (
-    SELECT (
-            SELECT m1.NAME
-            FROM ModelElement m1
-            WHERE m1.ElementHandle = m.ParentHandle
-        ) AS [Table Name],
-        m.NAME AS [mName],
-        m.AXid,
-        s.RECID,
-        M.ParentId,
-        s.TableId,
-        s.FIELDID,
-        S.NAME,
-        s.SQLNAME,
-        (
-            SELECT MAX(FieldId)
-            FROM AX2012DB..SQLDICTIONARY
-            WHERE SQLDICTIONARY.TableId = s.TableId
-        ) + 1 as [Next FieldId Would Be]
-    FROM ModelElement m
-    LEFT OUTER JOIN AX2012DB..SQLDICTIONARY s
-        ON m.ParentId = s.TABLEID
-        AND s.FIELDID = m.AxId
-    WHERE m.ElementType = 42 -- UtilElementType::TableField
-        AND (s.ARRAY = 1 OR s.ARRAY IS NULL)
-        AND (s.FIELDID > 0 OR s.FIELDID IS NULL)
-        AND s.NAME !=  upper(m.NAME) collate Latin1_General_CI_AS
+	SELECT *,
+			(
+				SELECT MAX(FieldId)
+				FROM AX2012DB.dbo.SQLDICTIONARY
+				WHERE SQLDICTIONARY.TableId = TableId
+			) + 1 + RN_by_Table as [Next FieldId Would Be]
+	FROM (
+		SELECT (
+				SELECT m1.NAME
+				FROM ModelElement m1
+				WHERE m1.ElementHandle = m.ParentHandle
+			) AS [Table Name],
+			m.NAME AS [mName],
+			m.AXid,
+			s.RECID,
+			M.ParentId,
+			s.TableId,
+			s.FIELDID,
+			S.NAME,
+			s.SQLNAME,
+			ROW_NUMBER() OVER(PARTITION BY s.TableId ORDER BY RecID) as RN_by_Table
+		FROM ModelElement m
+		LEFT OUTER JOIN AX2012DB.dbo.SQLDICTIONARY s
+			ON m.ParentId = s.TABLEID
+			AND s.FIELDID = m.AxId
+		WHERE m.ElementType = 42 -- UtilElementType::TableField
+			AND (s.ARRAY = 1 OR s.ARRAY IS NULL)
+			AND (s.FIELDID > 0 OR s.FIELDID IS NULL)
+			AND s.NAME !=  upper(m.NAME) collate Latin1_General_CI_AS
+	) as SubQueryA
 )
 UPDATE AX2012DB.dbo.SQLDICTIONARY
 SET FieldID = [Next FieldId Would Be]
